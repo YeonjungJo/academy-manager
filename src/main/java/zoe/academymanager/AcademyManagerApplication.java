aa717d97cfdbf78bd07cc813184a209888027f1e
조연정 <yeonjung.jo@coupang.com>
2018-10-01 16:28:49 +0900

    [AM-000] first commit

---
+++ src/main/java/zoe/academymanager/AcademyManagerApplication.java
@@ -0,0 +1,12 @@
+package zoe.academymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademyManagerApplication.class, args);
+    }
+}
---
+++ src/test/java/zoe/academymanager/AcademyManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package zoe.academymanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AcademyManagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
